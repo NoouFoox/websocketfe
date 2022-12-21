0c7e880316a04273bbd3257f5f9ee7b9facb70e6
hejunhao <nougat_hx@qq.com>
2022-12-21 10:21:33 +0800

    !

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,8 +1,21 @@
 <script setup>
 import { Moon, Sunny } from "@element-plus/icons-vue";
 import { useDark } from "@vueuse/core";
+import { ref } from "vue";
 const isDark = useDark();
 const flagDark = () => isDark;
+const locemit = ref(false);
+const locclick = () => {
+  localStorage.setItem("##@@mit", JSON.stringify(locemit.value));
+};
+locclick();
+if (localStorage.getItem("##@@mit") !== null) {
+  if (localStorage.getItem("##@@mit") === "false") {
+    locemit.value = false;
+  } else {
+    locemit.value = true;
+  }
+}
 </script>
 <template>
   <el-container class="container">
@@ -12,16 +25,25 @@ const flagDark = () => isDark;
         <el-menu-item index="/about">About</el-menu-item>
         <el-menu-item :index="`/user/${'游客登录'}`">User</el-menu-item>
         <el-menu-item index="/chat">Chat</el-menu-item>
-        <el-switch
-        @change="flagDark"
-        v-model="isDark"
-        :active-icon="Moon"
-        :inactive-icon="Sunny"
-        class="container-header-switch"
-      />
+        <div class="container-header-switch">
+          通知:
+          <el-switch
+            @change="locclick"
+            v-model="locemit"
+            active-text="开启"
+            inactive-text="关闭"
+          />
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <el-switch
+            @change="flagDark"
+            v-model="isDark"
+            :active-icon="Moon"
+            :inactive-icon="Sunny"
+          />
+        </div>
       </el-menu>
     </el-header>
-    <el-main>
+    <el-main class="elmmm">
       <RouterView />
     </el-main>
   </el-container>
@@ -36,5 +58,8 @@ const flagDark = () => isDark;
   top: 50%;
   transform: translateY(-50%);
   right: 20px;
+}
+.elmmm {
+  width: 100%;
 }
 </style>
--- vite-project/src/view/ChatRoom.vue
+++ vite-project/src/view/ChatRoom.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { onMounted, reactive, ref, watch } from "vue";
-import { Plus } from "@element-plus/icons-vue";
+import dayjs from "dayjs";
 import { socket } from "../io";
 import { msg } from "../tool";
+import { ElMessage } from "element-plus";
+import push from "push.js";
+const msgon = ref();
 const userInfo = reactive({
   name: "",
   msg: "默认信息",
@@ -13,9 +16,21 @@ socket.on("connect", () => {
   userInfo.name = socket.id;
   msg.emsg("成功建立链接", `标识符为: ${socket.id}`);
 });
-
-socket.emit("uproomliet")
-socket.on("sendmsg", (d) => console.log("⬅️ " + d.name + ":" + d.msg));
+const msgList = reactive([]);
+socket.emit("uproomliet");
+socket.on("sendmsg", (d) => {
+  msgList.push({ name: d.name, msg: d.msg });
+  // console.log("⬅️ " + d.name + ":" + d.msg);
+  if (localStorage.getItem("##@@mit") === "true") {
+    push.create(d.name, {
+      body: d.msg,
+      timeout: 1500,
+    });
+  }
+  setTimeout(() => {
+    msgon.value.scrollTo(0, msgon.value.scrollHeight);
+  }, 1);
+});
 socket.on("getroom", (d) => {
   const result = [];
   Object.keys(d).forEach((i) => {
@@ -25,9 +40,32 @@ socket.on("getroom", (d) => {
   roomList.push(...result);
 });
 const ef = () => {
-  console.log("➡️ " + userInfo.name + ":" + userInfo.msg);
+  if (userInfo.name === "") {
+    ElMessage({
+      message: "请输入用户名",
+      type: "warning",
+    });
+    return;
+  }
+  if (userInfo.msg === "") {
+    ElMessage({
+      message: "请输入要发送的信息",
+      type: "warning",
+    });
+    return;
+  }
+  // console.log("➡️ " + userInfo.name + ":" + userInfo.msg);
+  msgList.push({
+    name: "(你)" + userInfo.name,
+    msg: userInfo.msg,
+    color: true,
+  });
   socket.emit("msg", userInfo);
   elinput.value.clear();
+  setTimeout(() => {
+    console.log(msgon.value.scrollHeight);
+    msgon.value.scrollTo(0, msgon.value.scrollHeight);
+  }, 1);
 };
 
 // 房间注册
@@ -47,8 +85,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <el-input type="number" v-model="roomId"></el-input>
+  <div class="main">
+    <!-- <el-input type="number" v-model="roomId"></el-input>
     <el-button type="success" :icon="Plus" @click="getRoom">
       新建房间/加入房间
     </el-button>
@@ -58,12 +96,29 @@ onMounted(() => {
           <p>在线人数: {{ c.list }}</p>
         </el-card>
       </el-col>
-    </el-row>
-    用户名:<el-input type="text" v-model="userInfo.name" /> 信息:
-    <div ref="inputbox">
-      <el-input type="text" v-model="userInfo.msg" ref="elinput" />
+    </el-row> -->
+
+    <div>
+      用户名:<el-input type="text" v-model="userInfo.name" /> 信息:
+      <div ref="inputbox">
+        <el-input type="text" v-model="userInfo.msg" ref="elinput" />
+      </div>
     </div>
-    <el-button @click="ef">发送</el-button>
+
+    <div class="msgon" ref="msgon">
+      <el-timeline>
+        <el-timeline-item
+          :timestamp="dayjs().format()"
+          v-for="(activity, index) in msgList"
+          :key="index"
+          ><span
+            :style="{ color: activity.color === true ? '#aa3322' : '#66771a' }"
+            >{{ activity.name }}</span
+          >: &nbsp;{{ activity.msg }}</el-timeline-item
+        >
+      </el-timeline>
+    </div>
+    <el-button class="btn" @click="ef">发送</el-button>
   </div>
 </template>
 
@@ -73,5 +128,21 @@ onMounted(() => {
 }
 .el-col-class {
   margin-top: 20px;
+}
+.main {
+  height: 100%;
+}
+.msgon {
+  overflow: auto;
+  margin-top: 40px;
+  height: 60vh;
+}
+.btn {
+  position: fixed;
+  bottom: 20px;
+  width: 60vw;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1000000;
 }
 </style>
